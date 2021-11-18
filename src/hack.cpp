6575afc1663f4b5b972543da4701cb84cb2b9d0d
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-11-18 17:37:50 -0300

    Add teleport hack

--- src/hack.cpp
+++ src/hack.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <sstream>
 #include <dlfcn.h>
 #include "classes.h"
+
+void Player::Chat(const char *msg)
+{
+    std::stringstream ss(msg);
+    std::string cmd;
+
+    ss >> cmd;
+    if(cmd[0] == 't' && cmd[1] == 'p')
+    {
+        float x, y, z;
+        ss >> x >> y >> z;
+        
+        Vector3 newPos = Vector3(x, y, z);
+        if(cmd[2] == 'r')
+        {
+            this->SetPosition(this->GetPosition() + newPos);
+        }
+        if(cmd[2] == 'a')
+        {
+            this->SetPosition(newPos);
+        }
+    }
+}
 
 void World::Tick(float delta)
 {
@@ -16,6 +40,6 @@ void World::Tick(float delta)
 
 bool Player::CanJump()
 {
-    //std::cout << "CanJump" << std::endl;
+    std::cout << this->GetPlayerName() << std::endl;
     return true;
 }
